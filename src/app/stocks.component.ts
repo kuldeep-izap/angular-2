3875257d58e0c10362b2ec63e2aff6849c8859ed
kuldeep-izap <kuldeep.spall@izap.in>
2017-04-01 18:51:27 +0530

    First commit

--- src/app/stocks.component.ts
+++ src/app/stocks.component.ts
@@ -5,19 +5,21 @@ import { StockService} from './stock.service';
     selector: 'stocks',
     template: `<h2>Stocks</h2>
     {{title}}
-
+    Test
     <ul>
         <li *ngFor="let stock of stocks">
             {{stock}}
         </li>
     </ul>
+
     `
 })
 
 export class StocksComponent{
+    
     title = 'List of Stocks: ';
-    //stocks=['AAPL', 'IBM', 'GOOG', 'HanjiHello'];
     stocks;
+    
     constructor(stockservice : StockService){
         this.stocks = stockservice.getStocks();
     }
